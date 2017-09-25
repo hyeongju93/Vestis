e5729466b94b5ce9dc095d875ad2b87705e4b22b
Esc <pon02039@gmail.com>
2017-09-25 11:52:51 +0900

    조은솔 - 날씨 데이터 저장용 Vo 생성

---
+++ src/main/java/com/vestis/vo/CodiWeatherVo.java
@@ -0,0 +1,49 @@
+package com.vestis.vo;
+
+public class CodiWeatherVo {
+	private int no;
+	private int weatherNo;
+	private int temp;
+	
+	public CodiWeatherVo() {}
+
+	public CodiWeatherVo(int weatherNo, int temp) {
+		this.weatherNo = weatherNo;
+		this.temp = temp;
+	}
+
+	public CodiWeatherVo(int no, int weatherNo, int temp) {
+		this.no = no;
+		this.weatherNo = weatherNo;
+		this.temp = temp;
+	}
+
+	public int getNo() {
+		return no;
+	}
+
+	public void setNo(int no) {
+		this.no = no;
+	}
+
+	public int getWeatherNo() {
+		return weatherNo;
+	}
+
+	public void setWeatherNo(int weatherNo) {
+		this.weatherNo = weatherNo;
+	}
+
+	public int getTemp() {
+		return temp;
+	}
+
+	public void setTemp(int temp) {
+		this.temp = temp;
+	}
+
+	@Override
+	public String toString() {
+		return "CodiWeatherVo [no=" + no + ", weatherNo=" + weatherNo + ", temp=" + temp + "]";
+	}
+}
